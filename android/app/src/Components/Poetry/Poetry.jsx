2bd6038e85d423ae41cf76d507cc5518b58b4bc8
Mudasir <mudasir.bhatti358@gmail.com>
2024-09-20 10:50:13 +0500

    whatsapp share integrated

--- android/app/src/Components/Poetry/Poetry.jsx
+++ android/app/src/Components/Poetry/Poetry.jsx
@@ -1,4 +1,4 @@
-import { Alert, StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, Linking } from 'react-native';
 import React, { useState } from 'react';
 import { colors } from '../../utilities/colors';
 import IconWithText from '../(lite)/IconWithText';
@@ -7,7 +7,8 @@ import HeartIcon from '../../assets/icons/HeartIcon';
 import ShareIcon from '../../assets/icons/ShareIcon';
 import PoetryTextWithAuthor from '../(lite)/PoetryTextWithAuthor';
 import Clipboard from '@react-native-clipboard/clipboard';
-import Share from 'react-native-share';
+import Share, { Social } from 'react-native-share';
+import WhatsappIcon from '../../assets/icons/WhatsappIcon';
 
 const Poetry = ({ poetryTextArr, poet }) => {
     const [copiedText, setcopiedText] = useState('');
@@ -20,19 +21,17 @@ const Poetry = ({ poetryTextArr, poet }) => {
         Alert.alert(`سٹیٹس کاپی ہو گیا ہے ✔️`)
     };
 
+    // share with only whatsapp
     const whatsapShare = async () => {
-        const shareOptions = {
-            message: poetryTextArr.join('\n')
-        }
+        const message = encodeURIComponent(poetryTextArr.join('\n'));
+        const whatsappUrl = `whatsapp://send?text=${message}`;
 
         try {
-            await Share.open(shareOptions)
+            await Linking.openURL(whatsappUrl);
         } catch (error) {
-            console.log(`Failed to share error`)
+            console.log(`Failed to share: ${error.message}`);
         }
-    }
-    console.log(poetryTextArr)
-
+    };
 
     return (
         <View style={styles.mainContainer}>
@@ -41,7 +40,7 @@ const Poetry = ({ poetryTextArr, poet }) => {
             </View>
             <View style={styles.actionsWrapper}>
                 <IconWithText icon={<CopyIcon fill={colors.secondryClr} />} text={'Copy'} onPress={copyToClipboard} />
-                <IconWithText icon={<CopyIcon fill={'green'} />} text={'Whatsapp'} onPress={whatsapShare} />
+                <IconWithText icon={<WhatsappIcon />} text={'Whatsapp'} onPress={whatsapShare} />
                 <IconWithText icon={<HeartIcon fill={colors.alert} />} text={'Favourite'} />
                 <IconWithText icon={<ShareIcon fill={colors.secondryClr} />} text={'Share'} />
             </View>

e99389ee5dcb17996e320348d54627654407c8c1
Mudasir Bashir <for.all6474@gmail.com>
2024-11-21 00:59:41 +0500

    removed eslinting from the whole project

--- android/app/src/Components/ContentCompoent.jsx
+++ android/app/src/Components/ContentCompoent.jsx
@@ -1,40 +1,38 @@
-import { StyleSheet, View } from 'react-native'
-import React from 'react'
-import { colors } from '../utilities/colors'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {StyleSheet, View} from 'react-native';
+import React from 'react';
+import {colors} from '../utilities/colors';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-const ContentCompoent = ({ contentComponent }) => {
-    return (
-        <SafeAreaView style={styles.contentContainer}>
-            <View style={styles.whiteContainer}>
-                {contentComponent}
-            </View>
-        </SafeAreaView>
-    )
-}
+const ContentCompoent = ({contentComponent}) => {
+  return (
+    <SafeAreaView style={styles.contentContainer}>
+      <View style={styles.whiteContainer}>{contentComponent}</View>
+    </SafeAreaView>
+  );
+};
 
-export default ContentCompoent
+export default ContentCompoent;
 
 const styles = StyleSheet.create({
-    contentContainer: {
-        flex: 1,
-        width: '100%',
-        backgroundColor: colors.secondryClr,
-        position: 'relative',
-        overflow: 'hidden',
-        zIndex: 32,
-        marginTop: 208
-    },
-    whiteContainer: {
-        flex: 1,
-        height: '100%',
-        position: 'absolute',
-        width: '100%',
-        top: 0,
-        left: 0,
-        paddingVertical: 32,
-        paddingHorizontal: 24,
-        backgroundColor: colors.primaryClr,
-        borderTopRightRadius: 80,
-    }
-})
+  contentContainer: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: colors.secondryClr,
+    position: 'relative',
+    overflow: 'hidden',
+    zIndex: 32,
+    marginTop: 208,
+  },
+  whiteContainer: {
+    flex: 1,
+    height: '100%',
+    position: 'absolute',
+    width: '100%',
+    top: 0,
+    left: 0,
+    paddingVertical: 32,
+    paddingHorizontal: 24,
+    backgroundColor: colors.primaryClr,
+    borderTopRightRadius: 80,
+  },
+});
--- android/app/src/Components/HeaderTab.jsx
+++ android/app/src/Components/HeaderTab.jsx
@@ -1,60 +1,55 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import { colors } from '../utilities/colors'
-import { useNavigation } from '@react-navigation/native'
-import { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { setTabIndex } from '../reduxStore/features/counterSlice'
-const HeaderTab = ({ tabArray }) => {
+import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {colors} from '../utilities/colors';
+import {useNavigation} from '@react-navigation/native';
+import {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {setTabIndex} from '../reduxStore/features/counterSlice';
+const HeaderTab = ({tabArray}) => {
+  const navigation = useNavigation();
+  const myTab = useSelector(state => state.tab.tabIndex);
+  const dispatch = useDispatch();
 
-    const navigation = useNavigation()
-    const myTab = useSelector((state) => state.tab.tabIndex)
-    const dispatch = useDispatch()
+  const detectTab = index => {
+    dispatch(setTabIndex(index));
+  };
+  useEffect(() => {
+    navigation.navigate(tabArray[myTab].screen);
+  }, [myTab]);
+  return (
+    <View style={styles.tabContainer}>
+      {tabArray.map((comp, index) => (
+        <Pressable
+          key={index}
+          style={[
+            styles.categoryIcon,
+            myTab === index && {backgroundColor: colors.tabSelectedClr},
+          ]}
+          onPress={() => detectTab(index)}>
+          {comp.tabComp}
+        </Pressable>
+      ))}
+    </View>
+  );
+};
 
-    const detectTab = (index) => {
-        dispatch(setTabIndex(index))
-    }
-    useEffect(() => {
-        navigation.navigate(tabArray[myTab].screen)
-    }, [myTab])
-    return (
-        <View style={styles.tabContainer}>
-            {
-                tabArray.map((comp, index) =>
-                    <Pressable
-                        key={index}
-                        style={[
-                            styles.categoryIcon,
-                            myTab === index && { backgroundColor: colors.tabSelectedClr }
-                        ]
-                        }
-                        onPress={() => detectTab(index)}
-                    >
-                        {comp.tabComp}
-                    </Pressable>
-                )
-            }
-        </View >
-    )
-}
-
-export default HeaderTab
+export default HeaderTab;
 
 const styles = StyleSheet.create({
-    tabContainer: {
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        backgroundColor: colors.tabBgClr,
-        padding: 8,
-        borderRadius: 8
-    },
-    categoryIcon: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingVertical: 8,
-        paddingHorizontal: 36,
-        borderRadius: 8,
-    }
-})
+  tabContainer: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: colors.tabBgClr,
+    padding: 8,
+    borderRadius: 8,
+  },
+  categoryIcon: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 8,
+    paddingHorizontal: 36,
+    borderRadius: 8,
+  },
+});
